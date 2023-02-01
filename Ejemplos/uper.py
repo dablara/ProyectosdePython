f29ec9c2de05c493d422c3f62b17bc91eddb7fa7
Daniel <daniblanco707@gmail.com>
2023-02-01 18:49:59 +0100

    Primer comit

---
+++ Ejemplos/uper.py
@@ -0,0 +1,5 @@
+nombre = input('Introduzca  un nombre')
+
+mayus = nombre.upper(1)
+
+print(f'El nombre con letra minusculas es {nombre} y con mayusculas {mayus}')
---
+++ Ejerccios de clase/6.-grados.py
@@ -0,0 +1,14 @@
+ 
+
+
+
+
+# 5. Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
+
+print ('5. Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.')
+
+faren = int(input('¿Cuantos grados Fahrenheit hay? '))
+
+celcius = (faren - 32)* 5/9
+
+print (f'La conversion a grado celcius es {celcius}')
