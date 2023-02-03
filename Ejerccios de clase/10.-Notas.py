c78f5ee0f961117fccc1c6d919dbcb15505d4157
Daniel <daniblanco707@gmail.com>
2023-02-03 21:21:04 +0100

    3defebero

---
+++ Ejerccios de clase/10.-Notas.py
@@ -0,0 +1,33 @@
+#10. Un alumno desea saber cual será su calificación final en la materia de Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
+
+#* 55% del promedio de sus tres calificaciones parciales.
+
+#* 30% de la calificación del examen final.
+
+#* 15% de la calificación de un trabajo final
+
+nota1 = float(input('Introduce primera nota parcial '))
+nota2 = float(input('Introduce segunda nota parcial '))
+nota3 = float(input('Introduce tercera nota parcial '))
+nota4 = float(input('Introduce nota de examen final '))
+nota5 = float(input('Introduce nota de trabajo '))
+
+notasparciales= nota1+nota2+nota3
+promedio = notasparciales/3
+tres = 55*promedio/100
+notafinal = 30*nota4/100
+trabajo =  15*nota5/100
+evaluacion = tres+notafinal+trabajo
+
+#if (nota1,nota2,nota3,nota4,nota5) > 10:
+    #print ('Las notas deben estar comprendidas entre 0 y 10')
+#else: 
+    #print (f'Las notas de los parciales son {nota1},{nota2},{nota3} ')
+    #print (f'La nota del examen final es {nota4}')
+    #print (f'La nota del trabajo final es {nota5}')
+    #print (f'La nota final de la evaluación es {evaluacion}') 
+
+print (f'Las notas de los parciales son {nota1},{nota2},{nota3} ')
+print (f'La nota del examen final es {nota4}')
+print (f'La nota del trabajo final es {nota5}')
+print (f'La nota final de la evaluación es {evaluacion}') 
--- Ejerccios de clase/7.- Horas y minutos.py
+++ Ejerccios de clase/7.- Horas y minutos.py
@@ -1,14 +1,15 @@
 # 7. Realiza un programa que reciba una cantidad de minutos y 
 # muestre por pantalla a cuantas horas y minutos corresponde.
 
-import math
+
 from fractions import Fraction
 
 minutos = int(input('Introduce un número de minutos '))
 
-horas= minutos/60
-franccion = Fraction(horas)
-minu = franccion*60
+horas =  minutos/60
+fraccion = Fraction(horas)
+minu = fraccion*60
 
 
 print (f'{minutos} minutos son {horas} horas y {minu} minutos')
+
