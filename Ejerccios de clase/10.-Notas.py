afa4dd707833057a44154ac2a871ccaba202226a
Daniel <daniblanco707@gmail.com>
2023-03-07 22:09:10 +0100

    Ejercicios Actualizados

--- Ejerccios de clase/10.-Notas.py
+++ Ejerccios de clase/10.-Notas.py
@@ -1,9 +1,6 @@
 #10. Un alumno desea saber cual será su calificación final en la materia de Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
-
 #* 55% del promedio de sus tres calificaciones parciales.
-
 #* 30% de la calificación del examen final.
-
 #* 15% de la calificación de un trabajo final
 
 nota1 = int(input('Introduce primera nota parcial '))
--- Ejerccios de clase/9.- Tienda.py
+++ Ejerccios de clase/9.- Tienda.py
@@ -3,9 +3,7 @@
 
 precio =  float (input('Introduzca el precio de la compra '))
 
-
 ahorro = (15*precio)/100 #tambien podemos poner precio*0.15
 precio_final = precio - ahorro
-
 
 print (f'El precio base del producto es {precio}€ y el precio final con 15% de descuento es {precio_final:.2f}€ y nos hemos ahorrado {ahorro:.2f}€')
